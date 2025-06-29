4df5d0535f3fb43ee33cf2ec8cac10d00830b19c
Malcolm <kagsmalcolm@gmail.com>
2025-06-29 12:39:23 +0300

    Add server render

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
-// index.js
 const jsonServer = require('json-server');
+const cors = require('cors');
+
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
+
+server.use(cors());
+server.use(jsonServer.bodyParser);
 
 server.use(middlewares);
 server.use(router);
